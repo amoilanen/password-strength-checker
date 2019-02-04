daf067a962aa0d03ea174b15f41a68a7adb3d828
Anton Ivanov <anton.al.ivanov@gmail.com>
2019-02-04 23:49:06 +0200

    Implement function to check the strength of a password and unit tests for it

--- chalicelib/password_strength.py
+++ chalicelib/password_strength.py
@@ -1,3 +1,17 @@
-#TODO: Implement
+import re
+
+special_symbols = re.escape('½/!"#¤%&/()=?`@£$‚{[]}¸^*:;>\<|’̣¨~+-')
+patterns = [
+  '[a-z]+',
+  '\d+',
+  '[A-Z]+',
+  f'[{special_symbols}]+',
+  '.{8,}'
+]
+
 def compute_strength(password):
-    return 10
+    strength = 0
+    for pattern in patterns:
+        if len(re.findall(pattern, password)) > 0:
+            strength += 1
+    return strength * 2
--- test/password_strength_test.py
+++ test/password_strength_test.py
@@ -3,13 +3,23 @@ from chalicelib.password_strength import compute_strength
 
 class TestStringMethods(unittest.TestCase):
 
+    def test_empty_password(self):
+        self.assertEqual(compute_strength(''), 0)
+
     def test_all_digits(self):
-        self.assertEqual(compute_strength('123'), 0)
+        self.assertEqual(compute_strength('123'), 2)
 
-    def test_digits_and_some_letters(self):
-        self.assertEqual(compute_strength('123a'), 1)
+    def test_digits_letters(self):
+        self.assertEqual(compute_strength('123a'), 4)
 
-#TODO: Add more unit tests
+    def test_digits_letters_capital_letters(self):
+        self.assertEqual(compute_strength('123aB'), 6)
+
+    def test_digits_letters_capital_letters_special_symbols(self):
+        self.assertEqual(compute_strength('123aB#'), 8)
+
+    def test_digits_letters_capital_letters_special_symbols_long_enough(self):
+        self.assertEqual(compute_strength('123aB#1234'), 10)
 
 if __name__ == '__main__':
     unittest.main()
