af37cb7a21b4a5dafb9b76e9e6eef1c82635bb98
Anton Ivanov <anton.al.ivanov@gmail.com>
2019-02-05 23:01:04 +0100

    Return "BadRequest" when password is not provided

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from chalice import Chalice
+from chalice import Chalice, BadRequestError
 from chalicelib.password_strength import compute_strength
 import logging
 
@@ -9,12 +9,13 @@ app.log.setLevel(logging.DEBUG)
 def index():
     return {'hello': 'world'}
 
-#TODO: Handle invalid input, return BAD_REQUEST
 @app.route('/password/compute-strength', methods=['POST'],
            content_types=['application/json'])
 def password_compute_strength():
     request_body = app.current_request.json_body
     password = request_body.get('password')
+    if password is None:
+        raise BadRequestError('Password not provided')
     app.log.debug(f'Got password {password}')
     return {
         'strength': compute_strength(password)
